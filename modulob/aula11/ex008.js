2e60a2b5173dbc27da3404c7841e42c080c55541
FelipeFrancoT <feliperafrancotavares@gmail.com>
2026-03-18 14:38:21 -0400

    ex .js

---
+++ modulob/aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 59
+console.log(`a velocidade do seu carro e ${vel}`);
+if (vel > 60) {
+    console.log(`voçe ultrapacou o limite de velocidade.MULTADO!`)
+}
+
+console.log(`dirija sempre usando cinto de seguranca!`)
---
+++ modulob/aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = "eua"
+if(pais == "brasil"){
+    console.log("cria")
+}
+else{
+    console.log("estrageiro")
+}
